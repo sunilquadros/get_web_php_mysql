28d8928cc156cb2f63accdf54387b9078be293d1
root <root@workstation.lab.example.com>
2017-07-12 06:55:19 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# get_web_php_mysql
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+label{display:inline-block;width:100px;margin-bottom:10px;}
+</style>
+
+
+<title>Add Employee</title>
+</head>
+<body>
+
+<form method="get" action="file.php">
+<label>Instructor Number</label>
+<input type="text" name="instructorNumber" />
+<br />
+<label>Instructor Name</label>
+<input type="text" name="instructorName" />
+<br />
+<label>Email</label>
+<input type="text" name="email" />
+<br />
+<label>City</label>
+<input type="text" name="city" />
+<br />
+<label>State</label>
+<input type="text" name="state" />
+<br />
+<label>Postal Code</label>
+<input type="text" name="postalCode" />
+<br />
+<label>Country</label>
+<input type="text" name="country" />
+<br />
+</form>
+
+
+
+</body>
+</html>
